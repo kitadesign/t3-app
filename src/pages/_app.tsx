f9ea21fef4e4adddec07298d5a388cebe6e61c92
kitazawa <kitadesign@gmail.com>
2022-12-17 16:34:31 +0900

    path制御変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,11 @@ import { type AppType } from 'next/app'
 import { type Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
 
-import { trpc } from '../utils/trpc'
+import { trpc } from '@/utils/trpc'
 
-import '../styles/globals.css'
+import '@/styles/globals.css'
 
-const MyApp: AppType<{ session: Session | null }> = ({
+const App: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
@@ -17,4 +17,4 @@ const MyApp: AppType<{ session: Session | null }> = ({
   )
 }
 
-export default trpc.withTRPC(MyApp)
+export default trpc.withTRPC(App)
